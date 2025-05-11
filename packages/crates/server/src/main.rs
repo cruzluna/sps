f188f8ef07edebbda3cd263646d247e39db05cd6
Cruz <lunac8@unlv.nevada.edu>
2025-05-11 16:00:57 -0700

    wip: skeleton routes

--- packages/crates/server/src/main.rs
+++ packages/crates/server/src/main.rs
@@ -15,13 +15,72 @@ async fn hello_world() -> &'static str {
     "Hello, World!"
 }
 
+/// Get prompt
+#[utoipa::path(
+    get,
+    path = "/prompt",
+    responses(
+        (status = StatusCode::OK, description = "Successly retrieved prompt", body = String),
+        (status = StatusCode::NOT_FOUND, description = "Prompt not found"),
+        (status = StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error")
+    )
+)]
+#[axum_macros::debug_handler]
+async fn prompt() -> &'static str {
+    "Hello, World!"
+}
+
+/// Create prompt
+#[utoipa::path(
+    post,
+    path = "/prompt",
+    responses(
+        (status = StatusCode::CREATED, description = "Successly created prompt", body = String),
+        (status = StatusCode::BAD_REQUEST, description = "Invalid request body")
+    )
+)]
+#[axum_macros::debug_handler]
+async fn create_prompt() -> &'static str {
+    "Hello, World!"
+}
+
+/// Update prompt
+#[utoipa::path(
+    put,
+    path = "/prompt/{id}",
+    responses(
+        (status = StatusCode::OK, description = "Successly updated prompt", body = String),
+        (status = StatusCode::NOT_FOUND, description = "Prompt not found"),
+        (status = StatusCode::BAD_REQUEST, description = "Invalid request body")
+    )
+)]
+#[axum_macros::debug_handler]
+async fn update_prompt() -> &'static str {
+    "Hello, World!"
+}
+
+/// Delete prompt
+#[utoipa::path(
+    delete,
+    path = "/prompt/{id}",
+    responses(
+        (status = StatusCode::OK, description = "Successly deleted prompt"),
+        (status = StatusCode::NOT_FOUND, description = "Prompt not found")
+    )
+)]
+#[axum_macros::debug_handler]
+async fn delete_prompt() -> &'static str {
+    "Hello, World!"
+}
+
 #[derive(OpenApi)]
 #[openapi(
-    paths(hello_world),
+    paths(hello_world, prompt, create_prompt, update_prompt, delete_prompt),
     info(
-        title = "Hello World API",
-        version = "1.0.0",
-        description = "A simple hello world API"
+        title = "Simple Prompt Storage API",
+        version = "0.0.1",
+        description = "Simple prompt storage API that enables users to store and retrieve prompts, no longer requiring new deployments for
+        prompt updates."
     )
 )]
 struct ApiDoc;
